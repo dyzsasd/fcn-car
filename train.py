7bce0952348d11a41ab7514b3609b55ee6239514
szhang <szhang@szhang.com>
2017-08-23 15:50:51 +0000

    add random

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import os
 
 from keras.metrics import binary_accuracy
 from keras.optimizers import SGD
-from keras.preprocessing.image import resize_images, img_to_array, load_img, Iterator
+from keras.preprocessing.image import img_to_array, load_img, Iterator
 import numpy as np
 
 from res_model import get_model
@@ -22,7 +22,7 @@ class SegDirectoryIterator(Iterator):
         self.train_images = fnmatch.filter(
             os.listdir(self.train_path), '*.jpg')
         super(SegDirectoryIterator, self).__init__(
-            len(self.train_images), 10, True, None)
+            len(self.train_images), 10, True, 1)
 
     def next(self):
         with self.lock:
@@ -31,8 +31,8 @@ class SegDirectoryIterator(Iterator):
 
         # The transformation of images is not under thread lock so it can be
         # done in parallel
-        batch_x = np.zeros((current_batch_size, ) + (1280, 1918, 3))
-        batch_y = np.zeros((current_batch_size, ) + (1280, 1918, 1))
+        batch_x = np.zeros((current_batch_size, ) + (160, 240, 3))
+        batch_y = np.zeros((current_batch_size, ) + (160, 240, 1))
 
         for i, j in enumerate(index_array):
             data_file = self.train_images[j]
@@ -40,8 +40,8 @@ class SegDirectoryIterator(Iterator):
             img_y = load_img(os.path.join(
                 self.mask_path, data_file.replace(".jpg", "_mask.gif")))
 
-            img_x = resize_images(img_x, (160, 240))
-            img_y = resize_images(img_y, (160, 240))
+            img_x = img_x.resize((240, 160))
+            img_y = img_y.resize((240, 160))
 
             x = img_to_array(img_x)
             y = img_to_array(img_y)
@@ -56,7 +56,7 @@ class SegDirectoryIterator(Iterator):
         return (batch_x, batch_y)
 
 
-model = get_model((1280, 1918, 3))
+model = get_model((160, 240, 3))
 model.compile(
     loss=binary_crossentropy_with_logits,
     optimizer=SGD(lr=0.01 / 16, momentum=0.9),
@@ -65,5 +65,5 @@ model.compile(
 
 # model.fit_generator(SegDirectoryIterator(), steps_per_epoch=1000, epochs=10)
 sdi = SegDirectoryIterator()
-model.fit_generator(sdi, steps_per_epoch=10, epochs=100)
+model.fit_generator(sdi, steps_per_epoch=100, epochs=10)
 # model.fit(sdi.self.train_images_data, sdi.self.mask_images_data, batch_size=100, epochs=100)
