2ea8f9d74a764bfd9fa888bd81b01fd2c1bf8f4d
szhang <shuai.zhang@dailymotion.com>
2017-08-24 10:41:47 +0200

    fix reverse

--- res_model.py
+++ res_model.py
@@ -181,13 +181,13 @@ def get_model(
 
     # Block 5
     x = Conv2D(
-        512, (3, 3), activation='relu', padding='same',
+        1024, (3, 3), activation='relu', padding='same',
         name='block5_conv1', kernel_regularizer=l2(weight_decay))(x)
     x = Conv2D(
-        512, (3, 3), activation='relu', padding='same',
+        1024, (3, 3), activation='relu', padding='same',
         name='block5_conv2', kernel_regularizer=l2(weight_decay))(x)
     x = Conv2D(
-        512, (3, 3), activation='relu', padding='same',
+        1024, (3, 3), activation='relu', padding='same',
         name='block5_conv3', kernel_regularizer=l2(weight_decay))(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
@@ -196,18 +196,37 @@ def get_model(
         4096, (7, 7), activation='relu', padding='same',
         name='fc1', kernel_regularizer=l2(weight_decay))(x)
     x = Dropout(0.5)(x)
-    x = Conv2D(
-        4096, (1, 1), activation='relu', padding='same',
-        name='fc2', kernel_regularizer=l2(weight_decay))(x)
-    x = Dropout(0.5)(x)
 
-    # classifying layer
     x = Conv2D(
-        classes, (1, 1), kernel_initializer='he_normal', activation='linear',
-        padding='valid', strides=(1, 1),
+        classes, (1, 1), activation='relu', padding='same',
         kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2DTranspose(
+        classes, 2, kernel_initializer='glorot_uniform', activation='relu')(x)
 
-    x = BilinearUpSampling2D(size=(32, 32), target_size=input_shape[:2] + (1, ))(x)
+    x = Conv2D(
+        classes, (1, 1), activation='relu', padding='same',
+        kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2DTranspose(
+        classes, 2, kernel_initializer='glorot_uniform', activation='relu')(x)
+
+    x = Conv2D(
+        classes, (1, 1), activation='relu', padding='same',
+        kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2DTranspose(
+        classes, 2, kernel_initializer='glorot_uniform', activation='relu')(x)
+
+    x = Conv2D(
+        classes, (1, 1), activation='relu', padding='same',
+        kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2DTranspose(
+        classes, 2, kernel_initializer='glorot_uniform', activation='relu')(x)
+
+    x = Conv2D(
+        classes, (1, 1), activation='relu', padding='same',
+        kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2DTranspose(
+        classes, 2, kernel_initializer='glorot_uniform', activation='relu')(x)
+    # classifying layer
 
     model = Model(img_input, x)
 
